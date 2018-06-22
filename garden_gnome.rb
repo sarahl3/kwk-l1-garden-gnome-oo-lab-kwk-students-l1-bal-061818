92630b4f1d1686b05a53d83e008f3427a0531065
Sarah Li <sarcl@hotmail.com>
2018-06-22 20:06:23 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,9 +1,15 @@
 # Code your instances here
 class GardenGnome
+  attr_accessor :name,:age
    def initialize(personality)
      @personality = evil
-  attr_reader :name
-  attr_writer :name
+    end
+   def gluten_allergy
+     @gluten_allergy = gluten_allergy
+  end
+   def gluten_allergy
+     @gluten_allergy
   end
 end
 test_gnome = GardenGnome.new
+test_gnome1.name = "Carl the Crappy"
